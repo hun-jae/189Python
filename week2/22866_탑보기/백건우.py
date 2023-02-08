ec3fcb0beff914d5d1a19f2e2e34a8d7f9e504a5
baekgeonwoo <74844581+baekgeonwoo@users.noreply.github.com>
2023-02-08 14:18:16 +0900

    dfs구현 실패 시간초과

--- week2/22866_탑보기/백건우.py
+++ week2/22866_탑보기/백건우.py
@@ -1,35 +1,65 @@
 import sys
-from collections import deque
-
-input = sys.stdin.readline
+sys.setrecursionlimit(10**9)
 INF = sys.maxsize
+input = sys.stdin.readline
 N = int(input())
-buildings = deque(map(int,input().split()))
-canSee = [0 for _ in range(N)]
-minDis = [[INF,INF] for _ in range(N)]  #거리, 건물 번호
-for i in range(len(buildings)):         #완전 탐색으로 풀이할시 N*N 시간초과 발생
-    # 왼쪽 탐색
-    for j in range(i-1, -1, -1):
-        if buildings[i] <= buildings[j] :
-            break
-        elif buildings[i] > buildings[j] :
-            if i - j < minDis[j][0] :
-                minDis[j][0] = i - j
-                minDis[j][1] = i+1
-            canSee[j] += 1
-    # 오른쪽 탐색
-    for j in range(i+1,len(buildings)):
-        if buildings[i] <= buildings[j] :
-            break
-        elif buildings[i] > buildings[j] :
-            if j - i < minDis[j][0] :
-                minDis[j][0] = j - i
-                minDis[j][1] = i+1
-            canSee[j] += 1
+buildings = list(map(int,input().split()))
+isFind = [False for _ in range(N)]
+canSeeR = [0 for _ in range(N)]
+canSeeL = [0 for _ in range(N)]
+minDis = [[INF,0] for _ in range(N)] #거리, 번호
+
+def dfsRight(idx):
+    if isFind[idx] == True:
+        return canSeeR[idx]+1
+    isFind[idx] = True
+    for i in range(idx+1, N):
+        if buildings[idx] < buildings[i]:
+            if minDis[idx][0] > i - idx :
+                minDis[idx][0] = i - idx
+                minDis[idx][1] = i
+            canSeeR[idx] = dfsRight(i)
+            return canSeeR[idx] + 1
+    return canSeeR[idx]+1 # 오른쪽의 마지막 건물일 때
+
+def dfsLeft(idx):
+    if isFind[idx] == True:
+        return canSeeL[idx]+1
+    isFind[idx] = True
+    for i in range(idx-1, -1, -1):
+        if buildings[idx] < buildings[i]:
+            minDis[idx][0] = idx - i
+            minDis[idx][1] = i
+            canSeeL[idx] = dfsLeft(i)
+            return canSeeL[idx] + 1
+    return canSeeL[idx]+1 # 왼쪽의 마지막 건물일 때
+
+for i in range(len(buildings)-1,-1,-1):
+    if isFind[i] == False:
+        isFind[i] = True
+        for idx in range(i-1, -1,-1):
+            if buildings[i] < buildings[idx] :
+                minDis[i][0] = i - idx  #거리
+                minDis[i][1] = idx        #번호
+                canSeeL[i] = dfsLeft(idx)
+                break
+
+isFind = [False for _ in range(N)]
+for i in range(len(buildings)):
+    if isFind[i] == False:
+        isFind[i] = True
+        for idx in range(i+1, N):
+            if buildings[i] < buildings[idx] :
+                if minDis[i][0] > idx-i :
+                    minDis[i][0] = idx-i
+                    minDis[i][1] = idx
+                canSeeR[i] = dfsRight(idx)
+                break
 
 
-for idx, dis in enumerate(minDis):
-    if dis[0] == INF :
+for idx in range(N):
+    cnt = canSeeR[idx] + canSeeL[idx]
+    if cnt == 0:
         print("0")
     else :
-        print('{} {}'.format(canSee[idx],dis[1]))
+        print('{} {}'.format(cnt, minDis[idx][1]+1))
