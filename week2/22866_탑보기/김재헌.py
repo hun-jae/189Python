0fdb1bbc3b8b1654ad30c687ecefa05c54a7dfc3
hun-jae <61400572+hun-jae@users.noreply.github.com>
2023-02-14 09:26:15 +0900

    [Comment] : comment about code

--- week2/22866_탑보기/김재헌.py
+++ week2/22866_탑보기/김재헌.py
@@ -6,24 +6,27 @@ buildings = list(map(int, stdin.readline().split()))
 # 스택을 쌓을 때 result에 idx를 idx차이를 min으로 주면(오른쪽에만) idx를 구할 수 있음
 
 result = [200000] * n
+
+#왼쪽
 stack = []
-resultCntLeft = [0] * n
+resultCntLeft = [0] * n #왼쪽에 보이는 건물 수
 for idx, i in enumerate(buildings):
-    while stack and stack[-1][1] < i:
+    while stack and stack[-1][1] < i: #자신보다 작은 빌딩이 스택 top에 있으면 빼주고, 자신을 상대 빌딩의 result에 추가
         popIdx, popI = stack.pop()
         result[popIdx] = idx
-    if len(stack) > 0:
+    if len(stack) > 0: #작은 빌딩을 다 뺐는데 빌딩이 남아있다면
+        #자신과 같은 높이의 빌딩이 있을 수 있으므로 자신보다 큰 빌딩을 찾아야함
         k = -1
-        for k in range(len(stack) - 1, -1, -1):
-            if i < stack[k][1]:
+        for k in range(len(stack) - 1, -1, -1): 
+            if i < stack[k][1]: #자신보다 높은 빌딩의 stack에서의 index : k
                 break
-        if stack[k][1] > i:
-            k = stack[k][0]
-            resultCntLeft[idx] = resultCntLeft[k] + 1
+        if stack[k][1] > i: #디버깅하다가 추가된 코든데 최적화를 하지 못함 (자신보다 높은 빌딩이 스택에 존재할 때)
+            k = stack[k][0] #자신보다 높은 빌딩의 실제 index를 k에 저장
+            resultCntLeft[idx] = resultCntLeft[k] + 1 #자신의 왼쪽으로 보이는 건물 수
 
     stack.append([idx, i])
 
-# 반대편
+# 오른쪽
 # 반대로하는건 뒤집은 뒤 idx = n-1 - idx
 buildings = buildings[::-1]
 stack = []
@@ -45,10 +48,8 @@ for idx, i in enumerate(buildings):
     stack.append([idx, i])
 
 for i in range(n):
-    if result[i] == 200000: #아무도 못본친구
+    if result[i] == 200000: #아무도 못본 빌딩이면 0만 출력
         print(0)
     else:
         print(resultCntLeft[i] + resultCntRight[i], end=" ")
-        #print(resultCntLeft[i], end=" ")
-        #print(resultCntRight[i], end=" ")
         print(result[i] + 1)
