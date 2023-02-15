5795e84ae65cd5a826b40f2a3ecd2097ee634286
baekgeonwoo <74844581+baekgeonwoo@users.noreply.github.com>
2023-02-15 12:13:59 +0900

    위기 기존풀이 아래가 이분탐색으로 푼것 pypy로만 통과가능

--- week3/2776_암기왕/백건우.py
+++ week3/2776_암기왕/백건우.py
@@ -1,14 +1,42 @@
-import sys
-input = sys.stdin.readline
+# import sys
+# input = sys.stdin.readline
 
-T = int(input())        # 테스트 케이스의 개수
+# T = int(input())        # 테스트 케이스의 개수
+# for _ in range(T):
+#     N = int(input())
+#     yeon = set(map(int, input().split())) # set으로 입력받아야 시간초과 안남
+#     M = int(input())
+#     diary = list(map(int, input().split()))
+#     for num in diary: # diary를 순회해 set안에 존재하는지 찾는다.
+#         if num in yeon :
+#             print("1")
+#         else:
+#             print("0")
+
+import sys
+
+input = sys.stdin.readline
+T = int(input())
 for _ in range(T):
     N = int(input())
-    yeon = set(map(int, input().split())) # set으로 입력받아야 시간초과 안남
+    yeon = list(map(int, input().split()))
+    yeon.sort()
     M = int(input())
     diary = list(map(int, input().split()))
-    for num in diary: # diary를 순회해 set안에 존재하는지 찾는다.
-        if num in yeon :
+    for num in diary:
+        l = 0
+        r = len(yeon)-1
+        isIn = False
+        while l <= r:
+            mid = (l+r)//2
+            if yeon[mid] == num :
+                isIn = True
+                break
+            elif yeon[mid] > num :
+                r = mid-1
+            else :
+                l = mid +1
+        if isIn :
             print("1")
-        else:
+        else :
             print("0")
