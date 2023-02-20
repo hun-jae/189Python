8f67a386aef79ba40126c01bb5b8161559e99a5d
zogak <zogak7@gmail.com>
2023-02-20 20:31:52 +0900

    수정

--- week3/16434_드래곤_앤_던전/장윤아.py
+++ week3/16434_드래곤_앤_던전/장윤아.py
@@ -1,7 +1,8 @@
 #아직 푸는중....
+from math import ceil
 n, hATK = map(int, input().split())
 roomInfo = [list(map(int, input().split())) for _ in range(n)]
-left, right = 0, int(1e9)
+left, right = 1, int(1e9)
 answer = 0
 while left <= right:
     maxHP = (left+right)//2
@@ -20,13 +21,15 @@ while left <= right:
                     flag = False
                     break
 
-        if room[0] == 2: #포션
+        elif room[0] == 2: #포션
             portionATK, portionHP = room[1], room[2]
             hATK += portionATK
             curHP += portionHP
             if curHP > maxHP:
-                maxHP = curHP
+                curHP = maxHP
 
+        if not flag:
+            break
 
     if flag: # 용사 안죽음
         answer = maxHP
