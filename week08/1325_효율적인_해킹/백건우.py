ddef51e953b1a5eb58e1bb5376f4c86371c4bb05
baekgeonwoo <74844581+baekgeonwoo@users.noreply.github.com>
2023-03-22 09:46:11 +0900

    Create 백건우.py

---
+++ week08/1325_효율적인_해킹/백건우.py
@@ -0,0 +1,39 @@
+# bfs로 풀었는데 pypy로만 통과함 dfs로 바꿔서 다시 풀 예정
+
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+tree = [[] for _ in range(n+1)]
+for _ in range(m) :
+    a, b = map(int, input().split())
+    tree[b].append(a)
+
+def bfs(idx):
+    global cnt
+    visit[idx] = True
+    q = deque([idx])
+    cnt += 1
+    while q:
+        cur = q.popleft()
+        for next in tree[cur]:
+            if not visit[next]:
+                visit[next] = True
+                cnt += 1
+                q.append(next)
+
+maxHacked = 0
+ans = []
+for i in range(1, n+1):
+    visit = [False for _ in range(n+1)]
+    cnt = 0
+    bfs(i)
+    if maxHacked < cnt:
+        maxHacked = cnt
+        ans = [i]
+    elif maxHacked == cnt:
+        ans.append(i)
+
+print(*sorted(ans))
