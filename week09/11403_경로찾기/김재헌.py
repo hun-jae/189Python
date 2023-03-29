d54e885db81357bdb39ecad795e9e7fbe8e06bff
김재헌 <tint1998@gmail.com>
2023-03-29 10:47:37 +0900

    [refactor] : make to shortcode

--- week09/11403_경로찾기/김재헌.py
+++ week09/11403_경로찾기/김재헌.py
@@ -6,3 +6,12 @@ for m in n:
             g[s][t]=g[s][t]|(g[s][m]&g[m][t])
 for l in g:
     print(*l)
+
+# n = range(int(input()))
+# graph = [list(map(int, input().split())) for _ in n]
+# for m in n:
+#     for s in n:
+#         for t in n:
+#             graph[s][t] = graph[s][t] or (graph[s][m] and graph[m][t])
+# for l in graph:
+#     print(*l)
