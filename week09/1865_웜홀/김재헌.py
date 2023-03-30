d9aa67dde9441a806233b04deae1f2201c60c935
김재헌 <tint1998@gmail.com>
2023-03-30 13:49:34 +0900

    [Add] : solve 파티

--- week09/1865_웜홀/김재헌.py
+++ week09/1865_웜홀/김재헌.py
@@ -1,6 +1,8 @@
 for tc in range(int(input())):
     n, m, w = map(int, input().split())
     edges = []
+    #INF로 초기화 했을 때 각 노드의 묶음? 마다 하나의 노드의 본인 distance를 0으로 초기화해야함
+    #어차피 음수사이클의 존재만 판단하면 되기때문에 0으로 초기화해도 가능
     distance = [0] * (n + 1)
 
     for _ in range(m):
