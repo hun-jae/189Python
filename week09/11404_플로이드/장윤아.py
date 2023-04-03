3cc885b0a4bec2c4f28ab0398ca14785706de179
zogak <76643037+zogak@users.noreply.github.com>
2023-04-03 10:29:01 +0900

    Create 장윤아.py

---
+++ week09/11404_플로이드/장윤아.py
@@ -0,0 +1,21 @@
+n = int(input())
+m = int(input())
+
+distance = [[int(1e9)]*(n+1) for _ in range(n+1)]
+
+for _ in range(m):
+    a, b, cost = map(int, input().split())
+    distance[a][b] = min(distance[a][b], cost)
+
+for i in range(1, n+1):
+    distance[i][i] = 0
+
+for k in range(1, n+1):
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            distance[i][j] = min(distance[i][k]+distance[k][j], distance[i][j])
+
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        print(distance[i][j] if distance[i][j]!=int(1e9) else 0, end=' ')
+    print()
