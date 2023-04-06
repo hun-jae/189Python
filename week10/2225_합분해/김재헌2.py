2e3f8934f219ef0aa6ec90714990e2b081946b90
김재헌 <tint1998@gmail.com>
2023-04-06 14:08:16 +0900

    remove var prefix_sum

--- week10/2225_합분해/김재헌2.py
+++ week10/2225_합분해/김재헌2.py
@@ -1,8 +1,9 @@
-# 누적합 사용      cnt없애려면 맨앞열에 0 추가해줘야함(굳이?)
+# 누적합 사용
+# prefix_sum없애려면 맨앞열에 0 추가해줘야함(굳이?)
+# -> 0번부터 말고 1번부터 하면 prefix_sum 변수 없앨 수 있음
 N, K = map(int, input().split())
 dp = [[1 for _ in range(N+1)] for _ in range(K)]
 for i in range(1, K):
-    prefix_sum = 0
-    for j in range(N+1):
-        prefix_sum = dp[i][j] = prefix_sum + dp[i-1][j]
+    for j in range(1, N+1):
+        dp[i][j] = dp[i][j-1] + dp[i-1][j]
 print(dp[K-1][N] % 1000000000)
