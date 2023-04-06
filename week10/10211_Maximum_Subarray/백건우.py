93bc2df6398aa6d84efe2c355800bd152163135b
baekgeonwoo <74844581+baekgeonwoo@users.noreply.github.com>
2023-04-06 13:06:33 +0900

    Create 백건우.py

---
+++ week10/10211_Maximum_Subarray/백건우.py
@@ -0,0 +1,10 @@
+tc = int(input())
+
+for t in range(tc):
+    n = int(input())
+    nums = list(map(int,input().split()))
+    dp = [0 for _ in range(n)]
+    dp[0] = nums[0]
+    for i in range(1, n):
+        dp[i] = max(nums[i], dp[i-1]+nums[i])
+    print(max(dp))
