b947b282466a01bfd1ecc21adc035163db813173
baekgeonwoo <74844581+baekgeonwoo@users.noreply.github.com>
2023-04-10 11:32:29 +0900

    Update 백건우.py

--- week10/11722_가장_긴_감소하는_부분수열/백건우.py
+++ week10/11722_가장_긴_감소하는_부분수열/백건우.py
@@ -1,26 +1,16 @@
-# lower_bound함수를 bisect 함수를 써서 풀려고 했는데 뭔가가 안됨
 # 12738 가장 긴 증가하는 부분 수열 3 푸는 것처럼 품
 # 배열 뒤집어서 가장 긴 증가하는 부분 수열 = 원래 배열의 가장 긴 감소하는 부분 수열
-def lower_bound(target):
-    left, right = 0, len(ans)
-
-    while left < right:  # left와 right가 만나는 지점이 target값 이상이 처음 나오는 위치
-        mid = (right + left) // 2
-
-        if ans[mid] < target:
-            left = mid + 1
-        else:
-            right = mid
-
-    return right
+import bisect as bs
 
 n = int(input())
 nums = list(map(int,input().split()))
-ans = [1001]
-for num in reversed(nums):
-    if num > ans[-1]:
+ans = []
+for num in nums[::-1]: # 배열을 뒤 부터 접근
+    if not ans:
         ans.append(num)
     else:
-        idx = lower_bound(num)
-        ans[idx] = num
+        if num > ans[-1]:
+            ans.append(num)
+        else:
+            ans[bs.bisect_left(ans,num)] = num
 print(len(ans))
