136f9f45b0864ef36258f488b1b7bbe161030cf2
baekgeonwoo <74844581+baekgeonwoo@users.noreply.github.com>
2023-04-14 12:02:00 +0900

    Update 백건우.py

--- week11/20442_ㅋㅋ루ㅋㅋ/백건우.py
+++ week11/20442_ㅋㅋ루ㅋㅋ/백건우.py
@@ -2,8 +2,8 @@
 KKRKK -> R 양 끝에 K가 2개씩 붙어있으니까 5
 RRKRR -> 중간의 K를 제외하면 총 R의 개수가 4임
 중간에 있는 글자를 몇개 지워서 가장 긴 대칭 수열을 찾는다.
-실패 코드
-"""
+실패 코드 - 아이디어는 맞았는데 투포인터 구현부분을 잘못해서 시간초과남
+
 import sys
 
 input = sys.stdin.readline
@@ -31,4 +31,30 @@ for i in range(len(s)):
             if s[j] == 'R':
                 cntR += 1
                 ans = max(ans, cntR + 2*min(right[i], left[j]))
+print(ans)
+"""
+import sys
+
+input = sys.stdin.readline
+s = list(input())
+
+totalK = s.count('K')
+cnt = 0
+left = []
+right = []
+for i in range(len(s)):
+    if s[i] == 'K':
+        cnt += 1
+    elif s[i] == 'R':
+        left.append(cnt)
+        right.append(totalK - cnt)
+
+ans = 0
+l, r = 0, len(right) - 1   # 넓은 범위에서 줄여나간다.
+while l <= r :
+    ans = max(ans, r - l + 1 + 2 * min(left[l],right[r]))
+    if left[l] < right[r]:   # left[l]이 right[r]보다 작으면 r을 아무리 줄여도 결국 최대로 가능한 k의 개수는 left[l]이다. 따라서 left[l]을 더 크게 만들어서 가능한 k의 개수를 더 늘려야 한다.
+        l += 1
+    else :  # 위와 
+        r -= 1
 print(ans)
