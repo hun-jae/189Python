10fd1c597bc44315a43b2566cef1e5e568feb1cd
baekgeonwoo <74844581+baekgeonwoo@users.noreply.github.com>
2023-04-14 09:14:42 +0900

    Create 백건우.py

---
+++ week11/14246_K보다_큰_구간/백건우.py
@@ -0,0 +1,23 @@
+/**
+*  pypy로만 통과. 현재 right에서 구간의 합이 k를 넘겼다면 이후의 right가 가리키는 수가 전부 양수이므로 이후의 right모두 통과한다.
+*/
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+nums = list(map(int,input().split()))
+k = int(input())
+
+left, right = 0, 0
+ans = 0
+while right < n:
+    cur = sum(nums[left:right+1])
+    if cur > k :
+        ans += n - right
+        left += 1
+        if left > right :
+            right += 1
+    else :
+        right += 1
+print(ans)
