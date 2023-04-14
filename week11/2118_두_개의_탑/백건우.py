c0daa428b252014e2ae8ce5bf1a2fd926822f9a6
baekgeonwoo <74844581+baekgeonwoo@users.noreply.github.com>
2023-04-14 10:20:59 +0900

    Create 백건우.py

---
+++ week11/2118_두_개의_탑/백건우.py
@@ -0,0 +1,21 @@
+/**
+*  pypy로만 통과함. left랑 right를 어떤 기준으로 증가시킬지 몰라서 브루트포스 느낌으로 구현함.
+*/
+
+n = int(input())
+dis = [int(input()) for _ in range(n)]
+
+arr = [0 for _ in range(n)]
+for i in range(1, n):
+    arr[i] = arr[i-1] + dis[i-1]
+
+total = sum(dis)
+ans = 0
+for right in range(1, n):
+    for left in range(0, right):
+        asc = arr[right] - arr[left]
+        desc = total - asc
+        cur = min(asc, desc)
+        if ans < cur :
+            ans = cur
+print(ans)
